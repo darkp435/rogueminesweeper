26b0be1b847aaa4fad9dcd584781767ca94eb4a3
darkp435 <185438664+darkp435@users.noreply.github.com>
2026-08-07 15:36:57 +1200

    implement player movement

--- main.gd
+++ main.gd
@@ -46,7 +46,6 @@ func generate_grid(x: int, y: int, mines: int):
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
