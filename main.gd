921cf1cffb0ad11bdc43539e999f4a276ca914a5
darkp435 <185438664+darkp435@users.noreply.github.com>
2026-08-16 14:38:42 +1200

    add tile reveals

--- main.gd
+++ main.gd
@@ -10,16 +10,25 @@ enum SquareState {
 	Mine
 }
 
+# NOTE: this class may be moved to separate script in future.
 class SimpleGridCoord extends RefCounted:
 	var x: int
 	var y: int
 	
-	func _init(x: int, y: int):
-		self.x = x
-		self.y = y
+	func _init(ix: int, iy: int):
+		self.x = ix
+		self.y = iy
 	
 	func equal(other: SimpleGridCoord):
 		return self.x == other.x and self.y == other.y
+
+func get_node_from_coord(coord: SimpleGridCoord):
+	var path = "%d-%d" % [coord.y, coord.x]
+	return get_node(path)
+
+func grid_coord_from_name(grid_name: String):
+	var unpacked = grid_name.split("-")
+	return SimpleGridCoord.new(int(unpacked[1]), int(unpacked[0]))
 
 func generate_mine(x: int, y: int):
 	var mine_x = rng.randi_range(0, x - 1)
@@ -74,22 +83,47 @@ func _ready() -> void:
 	generate_grid(10, 10, 10)
 	draw_grid()
 
+func on_grid(x, y):
+	var length = len(grid[0])
+	var height = len(grid)
+	
+	return y > -1 and x > -1 and x < length and y < height
+
 func reveal(grid_coord: SimpleGridCoord):
+	var x = grid_coord.x
+	var y = grid_coord.y
 	var tile = grid[grid_coord.y][grid_coord.x]
+	
 	if tile == SquareState.Mine:
 		# The tile is a mine
 		return -1
+	
 	var mines_around = 0
 	for offset in grid_offsets:
-		
+		if on_grid(x + offset[1], y + offset[0]) and grid[y + offset[0]][x + offset[1]] == SquareState.Mine:
+			mines_around += 1
+	
+	return mines_around
 
-func grid_coord_from_name(name: String):
-	var unpacked = name.split("-")
-	return SimpleGridCoord.new(unpacked[1], unpacked[0])
+var display_map = {
+	# Swirl is the mine
+	-1: load("res://swirl.png"),
+	0: load("res://none.png"),
+	1: load("res://one.png"),
+	2: load("res://two.png"),
+	3: load("res://three.png"),
+	4: load("res://four.png"),
+	5: load("res://five.png"),
+	6: load("res://six.png"),
+	7: load("res://seven.png"),
+	8: load("res://eight.png")
+}
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_collision_detector_area_entered(area: Area2D) -> void:
-	pass
+	for child in area.get_children():
+		if child is Sprite2D:
+			child.texture = display_map[reveal(grid_coord_from_name(area.name))]
