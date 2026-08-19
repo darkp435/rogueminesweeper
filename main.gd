917cd9322dfa8f7e8cf62f7b36ff4723c5143cca
darkp435 <185438664+darkp435@users.noreply.github.com>
2026-08-19 18:35:54 +1200

    add infinite levels

--- main.gd
+++ main.gd
@@ -4,6 +4,15 @@ var grid = []
 var rng = RandomNumberGenerator.new()
 var sprite_texture = load("res://unrevealed.png")
 var collision_shape_rect = RectangleShape2D.new()
+var lives = 3
+var revealed = 0
+const game_over_scene = preload("res://Scenes/game_over.tscn")
+var grid_mines = 10
+var grid_x = 10
+var grid_y = 10
+var level = 1
+var flag = false
+var flagged_tiles = []
 
 enum SquareState {
 	Safe,
@@ -79,8 +88,11 @@ const grid_offsets = [
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#if not FileAccess.file_exists("res://2025 Introduction to Organic Chemistry.pptx"):
+		#OS.crash("FILE NOT FOUND")
 	collision_shape_rect.size = Vector2(32, 32)
-	generate_grid(10, 10, 10)
+	$CharacterBody2D.position = Vector2(32 * 5, 32 * 10)
+	generate_grid(grid_x, grid_y, grid_mines)
 	draw_grid()
 
 func on_grid(x, y):
@@ -88,6 +100,30 @@ func on_grid(x, y):
 	var height = len(grid)
 	
 	return y > -1 and x > -1 and x < length and y < height
+
+func destroy_grid():
+	for child in self.get_children():
+		if "-" in child.name:
+			child.queue_free()
+
+func redraw_remaining_label():
+	$CanvasLayer/Control/Remaining.text = "Tiles revealed: %d/%d" % [revealed, grid_x * grid_y - grid_mines]
+
+func next_level():
+	level += 1
+	destroy_grid()
+	$CanvasLayer/Control/Level.text = "Level " + str(level)
+	# Either width or height gets increased, randomly
+	if rng.randi_range(1, 2) == 1:
+		grid_x += 1
+	else:
+		grid_y += 1
+	
+	grid_mines += rng.randi_range(1, 5)
+	generate_grid(grid_x, grid_y, grid_mines)
+	draw_grid()
+	redraw_remaining_label()
+	revealed = 0
 
 func reveal(grid_coord: SimpleGridCoord):
 	var x = grid_coord.x
@@ -119,11 +155,43 @@ var display_map = {
 	8: load("res://eight.png")
 }
 
+const MINE = -1
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("flag"):
+		flag = not flag
 
 func _on_collision_detector_area_entered(area: Area2D) -> void:
-	for child in area.get_children():
-		if child is Sprite2D:
-			child.texture = display_map[reveal(grid_coord_from_name(area.name))]
+	var child = area.get_child(0)
+	var coord = grid_coord_from_name(area.name)
+	
+	if not child is Sprite2D or area.get_meta("revealed") or (not flag and area.name in flagged_tiles):
+		return
+	
+	if flag:
+		if area.name in flagged_tiles:
+			flagged_tiles.erase(area.name)
+			child.texture = load("res://unrevealed.png")
+		else:
+			
+		
+	var result = reveal(coord)
+	if result == MINE:
+		lives -= 1
+		if lives == 0:
+			var new_scene = game_over_scene.instantiate()
+			$CanvasLayer.add_child(new_scene)
+			$CharacterBody2D.speed = 0
+			$CharacterBody2D/Sprite2D.visible = false
+			
+		$CanvasLayer/Control/Lives.text = "Lives: " + str(lives)
+	else:
+		revealed += 1
+		redraw_remaining_label()
+		
+	child.texture = display_map[result]
+	area.set_meta("revealed", true)
+	
+	if revealed == grid_x * grid_y - grid_mines:
+		next_level()
